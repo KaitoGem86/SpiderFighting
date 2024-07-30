aeef98484827511d5d954d4e28733613387d32a2
ADMIN-PC\Admin <letuandat556@gmail.com>
2024-07-30 15:20:35 +0700

    add fighting so

--- Assets/MyGame/03_Scripts/03_5_Enemy/EnemyController.cs
+++ Assets/MyGame/03_Scripts/03_5_Enemy/EnemyController.cs
@@ -3,9 +3,16 @@ using UnityEngine;
 
 namespace Core.GamePlay.Enemy{
     public class EnemyController : MonoBehaviour, IHittedByPlayer{
+        private EnemySO _soController;
+
+        public void Init(EnemySO soConTroller){
+            _soController = soConTroller;
+        }
+
         public void HittedByPlayer()
         {
             Debug.Log("Hitted by player");
+            _soController.DespawnObject(this.gameObject);
         }
 
         public Transform TargetEnemy { get => this.transform; }
--- Assets/MyGame/03_Scripts/03_6_Misson/FightingQuest/FightingQuestStep.cs
+++ Assets/MyGame/03_Scripts/03_6_Misson/FightingQuest/FightingQuestStep.cs
@@ -1,12 +1,36 @@
+using System.Collections.Generic;
+using Core.GamePlay.Enemy;
 using UnityEngine;
 
-namespace Core.GamePlay.Mission{
-    public class FightingQuestStep : QuestStep{
-        
-        public void Update(){
-            if(Input.GetKeyDown(KeyCode.J))
+namespace Core.GamePlay.Mission
+{
+    [System.Serializable]
+    public class EnemyData
+    {
+        public EnemySO enemySO;
+        public int count;
+    }
+
+    public class FightingQuestStep : QuestStep
+    {
+        [SerializeField] List<EnemyData> enemyDatas;
+
+        public void Awake()
+        {
+            foreach (var enemyData in enemyDatas)
+            {
+                for (int i = 0; i < enemyData.count; i++)
+                {
+                    enemyData.enemySO.Spawn(new Vector3(Random.Range(-5, 5), 0, Random.Range(-5, 5)));
+                }
+            }
+        }
+
+        public void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.J))
                 FinishStep();
         }
-        
+
     }
 }
