03d78606e0fbcff700b9a4169a4a559a473817f1
ADMIN-PC\Admin <letuandat556@gmail.com>
2024-08-05 15:05:24 +0700

    add animation

---
+++ Assets/MyGame/03_Scripts/03_2_Player/State/AttackState/DodgeAction.cs
@@ -0,0 +1,8 @@
+namespace Core.GamePlay.Player{
+    public class DodgeAction : LocalmotionAction{
+        public override void Enter(ActionEnum actionBefore)
+        {
+            base.Enter(actionBefore);
+        }
+    }
+}
