49c162a24c4b6b8af35acfdd551b48e76f1ab19d
ADMIN-PC\Admin <letuandat556@gmail.com>
2024-08-14 18:45:52 +0700

    update black board

---
+++ Assets/MyGame/03_Scripts/03_5_Enemy/BaseEnemyBlackBoard.cs
@@ -0,0 +1,30 @@
+using Animancer;
+using Core.GamePlay.Support;
+using Extensions.SystemGame.AIFSM;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Core.GamePlay.Enemy
+{
+    public class BaseEnemyBlackBoard : BlackBoard
+    {
+        [Header("========= General =========")]
+        public EnemyController controller;
+        public Vector3 defaultPosition;
+        public Vector3 targetPosition;
+        public AnimancerComponent animancerComponent;
+
+        [Header("========= Movement =========")]
+        public NavMeshAgent navMeshAgent;
+        public float speed = 5f;
+
+        [Header("========= Attack =========")]
+        public float damage;
+        public float attackDelayTime = 5f;
+        public bool isReadyToAttack = false;
+        public float sightRange = 10f;
+        public float attackRange = 2f;
+        public IHitted targetToAttack;
+        public Vector3 targetPos => targetToAttack.TargetEnemy.position;
+    }
+}
--- Assets/MyGame/03_Scripts/03_5_Enemy/EnemyBlackBoard.cs
+++ Assets/MyGame/03_Scripts/03_5_Enemy/EnemyBlackBoard.cs
@@ -7,29 +7,11 @@ using UnityEngine.AI;
 
 namespace Core.GamePlay.Enemy
 {
-    public class EnemyBlackBoard : BlackBoard
+    public class EnemyBlackBoard : BaseEnemyBlackBoard
     {
         [Header("========= General =========")]
-        public EnemyController controller;
-        public Vector3 defaultPosition;
-        public Vector3 targetPosition;
-        public AnimancerComponent animancerComponent;
         public EnemyModel[] enemyModels;
         public EnemyModel currentEnemyModel;
-
-
-        [Header("========= Movement =========")]
-        public NavMeshAgent navMeshAgent;
-        public float speed = 5f;
-
-        [Header("========= Attack =========")]
-        public float damage;
-        public float attackDelayTime = 5f;
-        public bool isReadyToAttack = false;
-        public float sightRange = 10f;
-        public float attackRange = 2f;
-        public IHitted targetToAttack;
-        public Vector3 targetPos => targetToAttack.TargetEnemy.position;
 
         [Header("========= Weapon =========")]
         public EnemyWeaponController weaponController;
