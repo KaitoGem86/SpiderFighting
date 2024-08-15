837e647b941645c0aa80043a3e0dfabe32d898c7
ADMIN-PC\Admin <letuandat556@gmail.com>
2024-08-15 16:18:21 +0700

    update

--- Assets/MyGame/03_Scripts/03_5_Enemy/WeaponOriented/HandWeapon.cs
+++ Assets/MyGame/03_Scripts/03_5_Enemy/WeaponOriented/HandWeapon.cs
@@ -9,12 +9,6 @@ namespace Core.GamePlay.Enemy
         [SerializeField] private float _range;
         public override void OnWeaponAttack(Transform target)
         {
-            // _collider.enabled = true;
-            // Debug.DrawLine(transform.position, target.position, Color.red, 100);
-            // if(Vector3.Distance(transform.position, target.position) <= 0.5f)
-            // {
-            //     Debug.Log("HandWeapon Attack " + target.name);
-            // }
             var hit = Physics.OverlapSphere(transform.position, _range, _checkLayer);
             if(hit.Length > 0)
             {
@@ -27,14 +21,5 @@ namespace Core.GamePlay.Enemy
                 }
             }
         }
-
-        // public void OnTriggerEnter(Collider collider){
-        //     if (collider.CompareTag("Player"))
-        //     {
-        //         collider.GetComponent<IHitted>().HittedByPlayer(Extensions.SystemGame.AIFSM.FSMState.Hit);
-        //         _collider.enabled = false;
-        //     }
-            
-        // }
     }
 }
