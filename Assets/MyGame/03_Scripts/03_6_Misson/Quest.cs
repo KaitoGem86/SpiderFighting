08fc2f15aae38ebe3795c2ecdf28944c30caa4d1
ADMIN-PC\Admin <letuandat556@gmail.com>
2024-08-16 11:22:39 +0700

    complete quest loading

--- Assets/MyGame/03_Scripts/03_6_Misson/Quest.cs
+++ Assets/MyGame/03_Scripts/03_6_Misson/Quest.cs
@@ -1,38 +1,47 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-namespace Core.GamePlay.Mission{
+namespace Core.GamePlay.Mission
+{
     [CreateAssetMenu(fileName = "New Quest", menuName = "Quest/New Quest")]
-    public class Quest : ScriptableObject{
+    public class Quest : ScriptableObject
+    {
         public QuestInfor infor;
         public RewardInfor reward;
-        [SerializeField] private GameObject[] _stepPrefabs;
-        [SerializeField] private ScriptableObject[] _dataPrefabs;
-        
+        [SerializeField] private List<GameObject> _stepPrefabs;
+        [SerializeField] private List<ScriptableObject> _dataPrefabs;
+
         private IQuestStep[] _steps;
         private IMissionData[] _data;
 
         private int _currentStepIndex = 0;
 
-        public void Init(){
-            _steps = new IQuestStep[_stepPrefabs.Length];
-            _data = new IMissionData[_stepPrefabs.Length];
-            for (int i = 0; i < _stepPrefabs.Length; i++){
+        public void Init()
+        {
+            _steps = new IQuestStep[_stepPrefabs.Count];
+            _data = new IMissionData[_stepPrefabs.Count];
+            for (int i = 0; i < _stepPrefabs.Count; i++)
+            {
                 _steps[i] = _stepPrefabs[i].GetComponent<IQuestStep>();
             }
-            for (int i = 0; i < _dataPrefabs.Length; i++){
+            for (int i = 0; i < _dataPrefabs.Count; i++)
+            {
                 _data[i] = _dataPrefabs[i] as IMissionData;
             }
         }
 
-        public void StartQuest(){
+        public void StartQuest()
+        {
             _currentStepIndex = 0;
             var go = Instantiate(_steps[_currentStepIndex].GameObject);
             go.GetComponent<IQuestStep>().Init(this);
         }
 
-        public void NextQuestStep(){
+        public void NextQuestStep()
+        {
             _currentStepIndex++;
-            if (_currentStepIndex >= _steps.Length){
+            if (_currentStepIndex >= _steps.Length)
+            {
                 FinishQuest();
                 return;
             }
@@ -40,16 +49,31 @@ namespace Core.GamePlay.Mission{
             go.GetComponent<IQuestStep>().Init(this);
         }
 
-        public void FinishQuest(){
+        public void FinishQuest()
+        {
             Debug.Log("Finish Quest " + infor.QuestName);
             QuestManager.instance.NextQuest();
         }
 
-        public T GetData<T>() where T : IMissionData{
-            if(_data[_currentStepIndex] is T t){
+        public T GetData<T>() where T : IMissionData
+        {
+            if (_data[_currentStepIndex] is T t)
+            {
                 return t;
             }
             throw new System.Exception("Data type is not match");
+        }
+
+        public List<GameObject> stepPrefabs
+        {
+            get => _stepPrefabs;
+            set => _stepPrefabs = value;
+        }
+
+        public List<ScriptableObject> dataPrefabs
+        {
+            get => _dataPrefabs;
+            set => _dataPrefabs = value;
         }
     }
 }
--- Assets/MyGame/03_Scripts/03_6_Misson/QuestManager.cs
+++ Assets/MyGame/03_Scripts/03_6_Misson/QuestManager.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Collections.Generic;
+using AYellowpaper.SerializedCollections;
+using MyTools.ScreenSystem;
+using UnityEditor;
 using UnityEngine;
 
 namespace Core.GamePlay.Mission
@@ -7,7 +11,7 @@ namespace Core.GamePlay.Mission
     {
         public static QuestManager instance;
 
-        public Quest[] quests;
+        public List<Quest> quests;
         private Quest currentQuest;
         private int currentQuestIndex;
 
@@ -30,7 +34,7 @@ namespace Core.GamePlay.Mission
 
         public void NextQuest()
         {
-            currentQuestIndex = (currentQuestIndex + 1) % quests.Length;
+            currentQuestIndex = (currentQuestIndex + 1) % quests.Count;
             currentQuest = quests[currentQuestIndex];
             currentQuest.Init();
             currentQuest.StartQuest();
@@ -40,5 +44,121 @@ namespace Core.GamePlay.Mission
         {
             currentQuest.FinishQuest();
         }
+
+        #region  LOAD QUEST FROM SCENE
+#if UNITY_EDITOR
+        enum QuestType
+        {
+            Fighting,
+            Shipping,
+            StopCar,
+            Protected,
+            FightingBoss
+        }
+
+        enum QuestStepType
+        {
+            Receive,
+            Send,
+            Protect,
+            Shipping,
+            Fight,
+            FightBoss
+        }
+
+        [Header("Quest data storage")]
+        [SerializeField] private string questPath;
+        [SerializeField] private SerializedDictionary<QuestStepType, GameObject> questStepPrefabs;
+
+        [Header("Quests in scene")]
+        [SerializeField] private Transform questParent;
+
+        [ContextMenu("Load Quests")]
+        public void LoadQuests()
+        {
+            quests = new List<Quest>();
+            var tmp = GetDictLocation();
+            foreach (var quest in tmp)
+            {
+                GenerateQuestData(quest);
+            }
+        }
+
+        private List<KeyValuePair<QuestType, Transform>> GetDictLocation()
+        {
+            List<KeyValuePair<QuestType, Transform>> dict = new List<KeyValuePair<QuestType, Transform>>();
+            foreach (Transform child in questParent)
+            {
+                if (child.name.Contains("Boss"))
+                {
+                    dict.Add(new KeyValuePair<QuestType, Transform>(QuestType.FightingBoss, child));
+                }
+                else if (child.name.Contains("Fighting"))
+                {
+                    dict.Add(new KeyValuePair<QuestType, Transform>(QuestType.Fighting, child));
+                }
+                else if (child.name.Contains("Shipping"))
+                {
+                    dict.Add(new KeyValuePair<QuestType, Transform>(QuestType.Shipping, child));
+                }
+                else if (child.name.Contains("Stop Car"))
+                {
+                    dict.Add(new KeyValuePair<QuestType, Transform>(QuestType.StopCar, child));
+                }
+                else if (child.name.Contains("Protect"))
+                {
+                    dict.Add(new KeyValuePair<QuestType, Transform>(QuestType.Protected, child));
+                }
+            }
+            return dict;
+        }
+
+        private void GenerateQuestData(KeyValuePair<QuestType, Transform> questObject)
+        {
+            var folderPath = questPath + "/" + questObject.Value.name;
+            if (!AssetDatabase.IsValidFolder(folderPath))
+            {
+                AssetDatabase.CreateFolder(questPath, questObject.Value.name);
+            }
+            var receiveStepData = ScriptableObject.CreateInstance<ShippingQuestInitData>();
+            receiveStepData.position = questObject.Value.position;
+            AssetDatabase.CreateAsset(receiveStepData, folderPath + $"/Receive_{questObject.Value.name}.asset");
+            var questData = ScriptableObject.CreateInstance<Quest>();
+            questData.infor = new QuestInfor
+            {
+                QuestName = questObject.Value.name,
+                QuestDescription = "This is a quest"
+            };
+            questData.reward = new RewardInfor
+            {
+                exp = 100,
+                currency = 100
+            };
+            questData.stepPrefabs = new List<GameObject>();
+            questData.dataPrefabs = new List<ScriptableObject>();
+            questData.stepPrefabs.Add(questStepPrefabs[QuestStepType.Receive]);
+            questData.dataPrefabs.Add(receiveStepData);
+            switch (questObject.Key)
+            {
+                case QuestType.Fighting:
+                    break;
+                case QuestType.Shipping:
+                    foreach(Transform child in questObject.Value)
+                    {
+                        var shippingStepData = ScriptableObject.CreateInstance<ShippingQuestInitData>();
+                        shippingStepData.position = child.position;
+                        AssetDatabase.CreateAsset(shippingStepData, folderPath + $"/Shipping_{child.name}.asset");
+                        questData.stepPrefabs.Add(questStepPrefabs[QuestStepType.Shipping]);
+                        questData.dataPrefabs.Add(shippingStepData);
+                    }
+                    break;
+                default:
+                    break;
+            }
+            quests.Add(questData);
+            AssetDatabase.CreateAsset(questData, folderPath + $"/{questObject.Value.name}.asset");
+        }
+#endif
+        #endregion
     }
 }
