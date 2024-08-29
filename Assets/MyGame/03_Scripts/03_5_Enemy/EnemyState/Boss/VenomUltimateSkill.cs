5e0b0611e41c449fde841ad3e81d99baf0e7a474
ADMIN-PC\Admin <letuandat556@gmail.com>
2024-08-29 11:32:08 +0700

    complete venom

--- Assets/MyGame/03_Scripts/03_5_Enemy/EnemyState/Boss/VenomUltimateSkill.cs
+++ Assets/MyGame/03_Scripts/03_5_Enemy/EnemyState/Boss/VenomUltimateSkill.cs
@@ -1,5 +1,6 @@
 using Animancer;
 using Core.GamePlay.Support;
+using DG.Tweening;
 using Extensions.SystemGame.AIFSM;
 using UnityEngine;
 
@@ -14,12 +15,16 @@ namespace Core.GamePlay.Enemy
         {
             base.EnterState();
             _targetPos = _blackBoard.targetPos;
+            _blackBoard.navMeshAgent.ResetPath();
+            _blackBoard.navMeshAgent.isStopped = true;
+            _blackBoard.transform.DORotateQuaternion(Quaternion.LookRotation(_targetPos - _blackBoard.transform.position), 0.1f);
         }
 
         public void ApplyFirstDamage(int handIndex)
         {
             _blackBoard.weaponController.SetHandEnemy(handIndex == 0 ? _blackBoard.currentModel.leftHand : _blackBoard.currentModel.rightHand);
             _blackBoard.targetToAttack.TargetEnemy.GetComponent<IHitted>().ResponseClip = respone[2];
+            _blackBoard.targetToAttack.TargetEnemy.GetComponent<Rigidbody>().rotation = Quaternion.LookRotation(_blackBoard.transform.position - _blackBoard.targetToAttack.TargetEnemy.position);
             _blackBoard.weaponController.OnWeaponAttack(_blackBoard.targetToAttack.TargetEnemy, FSMState.ResponeForSpecialSkill);
         }
 
@@ -30,7 +35,7 @@ namespace Core.GamePlay.Enemy
 
         private void RotateToEnemy()
         {
-            _blackBoard.navMeshAgent.transform.rotation = Quaternion.Slerp(_blackBoard.navMeshAgent.transform.rotation, Quaternion.LookRotation(_targetPos - _blackBoard.navMeshAgent.transform.position), Time.deltaTime * 20);
+            //_blackBoard.navMeshAgent.transform.rotation = Quaternion.Slerp(_blackBoard.navMeshAgent.transform.rotation, Quaternion.LookRotation(_targetPos - _blackBoard.navMeshAgent.transform.position), Time.deltaTime * 20);
         }
 
         protected override int GetIndexTransition()
